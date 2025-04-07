5889615d512a3c00dd61371eb819659ad8c1d1bf
joaquinharguindeguy <jaharguindeguy@mail.austral.edu.ar>
2025-04-07 15:14:58 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,5 @@
 def slice_simple():
 	texto = "Awesome"
 	medio= (int(int(len(texto))/2))
-	b= print(f'\ntexto = {texto}\n{texto[:3]}\n{texto[medio-1 : medio+2]}\n{texto.lower()}')
+	b= print(f'\ntexto = {texto}\n\n{texto[:3]}\n{texto[medio-1 : medio+2]}\n{texto.lower()}')
 	
