ac060cda128204a6d1862427fe9939e819749d1b
joaquinharguindeguy <jaharguindeguy@mail.austral.edu.ar>
2025-04-07 20:12:29 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,7 +1,4 @@
 def check_vowels():
-	nombre = (input("> "))
-	print (f'\nContiene a: {"a" in nombre}\nContiene e: {"e" in nombre}\nContiene i: {"i" in nombre}\nContiene o: {"o" in nombre}\nContiene u: {"u" in nombre}\n')
-
 	name1= (f'\n{(input("> "))}')
 	name= (name1.lower())
 	print(f'\nContiene a: {"a" in name}\nContiene e: {"e" in name}\nContiene i: {"i" in name}\nContiene o: {"o" in name}\nContiene u: {"u" in name}\n')
