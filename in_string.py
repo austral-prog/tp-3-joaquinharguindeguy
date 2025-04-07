80442b217adb52f6be4c4727b9b7bb09c3471321
joaquinharguindeguy <jaharguindeguy@mail.austral.edu.ar>
2025-04-07 12:39:33 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,5 +1,13 @@
 def check_vowels():
-    # Código a implementar utilizando input.
+     nombre = (input("> "))
+	a=print (f'\nContiene a: {"a" in nombre}\nContiene e: {"e" in nombre}\nContiene i: {"i" in nombre}\nContiene o: {"o" in nombre}\nContiene u: {"u" in nombre}\n')
+
+	name1= (f'\n{(input("> "))}')
+	name= (name1.lower())
+	b= print(f'\nContiene a: {"a" in name}\nContiene e: {"e" in name}\nContiene i: {"i" in name}\nContiene o: {"o" in name}\nContiene u: {"u" in name}\n')
+
+    
+# Código a implementar utilizando input.
 
 
 # Para verificar este ejercicio ejecutar el comando
