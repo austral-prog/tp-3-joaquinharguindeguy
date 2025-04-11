36b5d6ddea11d1d9aab9ee5e1fccc3cc441b64e7
joaquinharguindeguy <jaharguindeguy@mail.austral.edu.ar>
2025-04-11 10:08:03 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,9 +1,6 @@
 def slice_advanced():
 	frase = (input("> "))
-	print(f'\n{frase [4::2]}\n\n')
-
-	numero= (input("> "))
-	print(f'\n{numero [4::2]}')
+	print(f'{frase [4::2]}')
 
 slice_advanced()
    
